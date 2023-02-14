c05e285b85d92fd22fd3f85232327066c5bae1cb
marllon <marllonlessa@gmail.com>
2023-02-14 17:44:24 -0300

    Database

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
 
 namespace WebApplication1
 {
@@ -33,7 +35,12 @@ namespace WebApplication1
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebApplication1Context>(options =>
+                options.UseMySql(Configuration.GetConnectionString("WebApplication1Context"), builder =>
+                builder.MigrationsAssembly("WebApplication1")));
         }
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
