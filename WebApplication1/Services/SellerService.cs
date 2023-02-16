d0a2f23dbc73911a0e6a8f897dc2d83c4f4c5771
marllon <marllonlessa@gmail.com>
2023-02-16 17:07:18 -0300

    Delete

--- WebApplication1/Services/SellerService.cs
+++ WebApplication1/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -28,6 +29,22 @@ namespace WebApplication1.Services
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+
+
+        }
+
+        
 
 
 
